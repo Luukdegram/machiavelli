812739e4bea5fe54804b944a1147df755ce305dc
Luuk de Gram <Luukdegram@gmail.com>
2016-01-04 16:33:17 +0100

    Improved Builder

--- models/character_cards/Builder.cpp
+++ models/character_cards/Builder.cpp
@@ -13,6 +13,10 @@ void Builder::doSpecialAbility(std::shared_ptr<Player> player, std::string comma
     player->getBuildingCards().push_back(gameController->getRandomBuildingCard());
     player->getBuildingCards().push_back(gameController->getRandomBuildingCard());
 
+    buildBuilding(player, 0);
+}
+
+void Builder::buildBuilding(std::shared_ptr<Player> player, int count) {
     player->getClient()->write("Do you want to build buildings?\r\n");
     player->getClient()->write("[0] Yes\r\n");
     player->getClient()->write("[1] No\r\n");
@@ -20,32 +24,28 @@ void Builder::doSpecialAbility(std::shared_ptr<Player> player, std::string comma
     int answer = stoi(player->getClient()->readline());
 
     if(answer == 0) {
-        buildBuilding(player, 0);
-    }
-}
+        shared_ptr<Socket> client = player->getClient();
 
-void Builder::buildBuilding(std::shared_ptr<Player> player, int count) {
-    shared_ptr<Socket> client = player->getClient();
+        client->write("Which building would you like to build? \r\n");
+        int counter = 1;
+        for (shared_ptr<BuildingCard> card : player->getBuildingCards()) {
+            client->write("[" + counter + "]" + card->getName() + "\r\n");
+            counter++;
+        }
 
-    client->write("Which building would you like to build? \r\n");
-    int counter = 1;
-    for(shared_ptr<BuildingCard> card : player->getBuildingCards()) {
-        client->write("[" + counter + "]" + card->getName() + "\r\n");
-        counter++;
-    }
+        answer = stoi(client->readline()) - 1;
 
-    int answer = stoi(client->readline()) - 1;
-
-    if(answer >= player->getBuildingCards().size()) {
-        client->write("Invalid option! \r\n");
-        buildBuilding(player, count);
-    }
+        if (answer >= player->getBuildingCards().size()) {
+            client->write("Invalid option! \r\n");
+            buildBuilding(player, count);
+        }
 
 
-    player->buildBuilding(player->getBuildingCards().at(answer));
-    count++;
+        player->buildBuilding(player->getBuildingCards().at(answer));
+        count++;
 
-    if(count < 3) {
-        buildBuilding(player, count);
+        if(count < 3) {
+            buildBuilding(player, count);
+        }
     }
 }
